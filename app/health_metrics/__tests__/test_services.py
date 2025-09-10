d604944687e3eff802038e86b5aba08ccf3a322b
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-10 17:01:57 -0400

    feat: implement service test suite.

--- app/health_metrics/__tests__/test_services.py
+++ app/health_metrics/__tests__/test_services.py
@@ -1,68 +1,64 @@
-from users.models import User
+from health_metrics.models import HealthMetric
 import pytest
 import uuid
 from django.forms.models import model_to_dict
-from users.service import UserService
-from .fixtures import user_factory, mock_user_data
+from health_metrics.service import HealthMetricService
+from .fixtures import health_metric_factory, mock_record_data
 
 class TestUserService:
-  def test_create_user(self, mocker, mock_user_data):
-    payload = mock_user_data.mock_user_in()
-    mock_user = mock_user_data.mock_user()
-    mocker.patch("users.service.UserRepository.create", return_value=mock_user)
+  def test_create(self, mocker, mock_record_data):
+    payload = mock_record_data.mock_user_in()
+    mock_record = mock_record_data.mock_record()
+    mocker.patch("health_metrics.service.HealthMetricRepository.create", return_value=mock_record)
 
-    user = UserService.create_user(payload)
+    record = HealthMetricService.create(payload)
 
-    assert user == mock_user
-
-
-  def test_get_user(self, mocker, mock_user_data):
-    mock_user = mock_user_data.mock_user()
-    mocker.patch("users.service.UserRepository.get_by_id", return_value=mock_user)
-
-    user = UserService.get_user(mock_user.id)
-
-    assert user == mock_user
+    assert record == mock_record
 
 
-  def test_list_users(self, mocker, user_factory):
-    user_factory.create()
-    qs = user_factory.queryset()        
+  def test_get(self, mocker, mock_record_data):
+    mock_record = mock_record_data.mock_record()
+    mocker.patch("health_metrics.service.HealthMetricRepository.get_by_id", return_value=mock_record)
 
-    mocker.patch("users.service.UserRepository.list_all", return_value=qs)
+    record = HealthMetricService.get(mock_record.id)
 
-    users = UserService.list_users()
-
-    assert users == qs
+    assert record == mock_record
 
 
-  def test_update_user(self, mocker, mock_user_data):
-    user = mock_user_data.mock_user()
-    mock_user_in = mock_user_data.mock_user_in()
+  def test_list(self, mocker, health_metric_factory):
+    health_metric_factory.create()
+    qs = health_metric_factory.queryset()        
 
-    mock_user_in.first_name = "John"    
+    mocker.patch("health_metrics.service.HealthMetricRepository.list_all", return_value=qs)
 
-    updated_user = User(
-        id=user.id,
-        first_name="John",
-        last_name=user.last_name,
-        email=user.email,
-        password=user.password,
-        role=user.role
-    )
+    records = HealthMetricService.list()
 
-    mocker.patch("users.service.UserRepository.update", return_value=updated_user)
-
-    result = UserService.update_user(user.id, mock_user_in)
-
-    assert result.email == updated_user.email
+    assert records == qs
 
 
-  def test_delete_user(self, mocker, mock_user_data):
-    mock_user = mock_user_data.mock_user()
+  def test_update(self, mocker, mock_record_data):
+    mock_record = mock_record_data.mock_record()
+    mock_record_in = mock_record_data.mock_user_in()
 
-    mocker.patch("users.service.UserRepository.delete")
+    # update through payload
+    mock_record_in.weight += 10
 
-    result = UserService.delete_user(mock_user.id)
+    # mock the update
+    updated_record = mock_record
+    mock_record.weight += 10
+
+    mocker.patch("health_metrics.service.HealthMetricRepository.update", return_value=updated_record)
+
+    result = HealthMetricService.update(mock_record.id, mock_record_in)
+
+    assert result.weight == updated_record.weight
+
+
+  def test_delete(self, mocker, mock_record_data):
+    mock_record = mock_record_data.mock_user()
+
+    mocker.patch("health_metrics.service.HealthMetricRepository.delete")
+
+    result = HealthMetricService.delete(mock_record.id)
 
     assert result is None
