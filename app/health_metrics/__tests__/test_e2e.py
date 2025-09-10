3a4b2c59ae7bb6e5ae5e88a1cbfc4935e1f369fb
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-09-10 17:19:10 -0400

    feat: implement e2e test suite.

--- app/health_metrics/__tests__/test_e2e.py
+++ app/health_metrics/__tests__/test_e2e.py
@@ -2,81 +2,104 @@ import json
 
 from django.forms import model_to_dict
 import pytest
-from users.models import User
-from users.schemas import UserIn
-from .fixtures import user_factory, mock_user_data
-from users.repository import UserRepository
+from health_metrics.models import HealthMetric
+from health_metrics.schemas import HealthMetricIn
+from .fixtures import health_metric_factory, mock_record_data
+from health_metrics.repository import HealthMetricRepository
 from core.tests.conftest import authenticated_client, mock_authenticate
 
 @pytest.mark.django_db
-class TestE2EUsers:
+class TestE2EHealthMetrics:
   
-  def test_create(self, authenticated_client, mock_user_data):
-    payload = mock_user_data.mock_payload()
+  def test_create(self, authenticated_client, mock_record_data):
+    payload = mock_record_data.mock_payload()
 
     response = authenticated_client.post(
-      "/api/users/",
+      "/api/health_metrics/",
       data=json.dumps(payload)
     )
     
     assert response.status_code == 200
 
 
-  def test_get(self, authenticated_client, user_factory):
-    user = user_factory.create()
-    user.save()
+  def test_get(self, authenticated_client, health_metric_factory):
+    record = health_metric_factory.create()
+    record.save()
 
     response = authenticated_client.get(
-      f"/api/users/{user.id}"      
+      f"/api/health_metrics/{record.id}"      
     )
     response_body = response.json()
 
     assert response.status_code == 200
-    assert response_body["email"] == user.email
+    assert response_body["id"] == record.id
 
 
-  def test_list(self, authenticated_client, user_factory):
-    user = user_factory.create()
-    user.save()
+  def test_list(self, authenticated_client, health_metric_factory):
+    record = health_metric_factory.create()
+    record.save()
         
-    user_list = user_factory.user_list()
+    record_list = health_metric_factory.list()
 
     response = authenticated_client.get(
-      "/api/users/"
+      "/api/health_metrics/"
     )
     response_body = response.json()
 
     assert response.status_code == 200
-    assert response_body[0]["email"] == user_list[0].email
+    assert response_body[0]["id"] == record_list[0].id
      
-  def test_update(self, authenticated_client, user_factory):
-    user = user_factory.create()
-    user.save()
+  def test_update(self, authenticated_client, health_metric_factory):
+    record = health_metric_factory.create()
+    record.save()
 
-    updated_user = {
-        "first_name": "John",
-        "last_name": user.last_name,
-        "email": user.email,
-        "password": user.password,
-        "role": user.role,
+    updated_record = {
+      "id": str(record.id),
+      "age": record.age,
+      "gender": record.gender,
+      "height": record.height,
+      "weight": record.weight + 10,
+      "bmi": record.bmi,
+
+      # results
+      "blood_pressure": record.blood_pressure,
+      "heart_rate": record.heart_rate,
+      "cholesterol": record.cholesterol,
+      "glucose": record.glucose,
+      "insulin": record.insulin,
+      "stress_level": record.stress_level,
+
+      # lifestyle choices
+      "sleep_hours": record.sleep_hours,
+      "sleep_quality": record.sleep_quality,
+      "work_hours": record.work_hours,
+      "physical_activity": record.physical_activity,
+      "daily_steps": record.daily_steps,
+      "calorie_intake": record.calorie_intake,
+      "alcohol_consumption": record.alcohol_consumption,
+      "smoking_level": record.smoking_level,
+      "water_intake": record.water_intake,
+      "diet_type": record.diet_type,
+      "exercise_type": record.exercise_type,
+      "sunlight_exposure": record.sunlight_exposure
     }
 
     response = authenticated_client.put(
-      f"/api/users/{user.id}",
-      data=json.dumps(updated_user)
+      f"/api/health_metrics/{record.id}",
+      data=json.dumps(updated_record)
     )
     response_body = response.json()
 
     assert response.status_code == 200
-    assert response_body["first_name"] == updated_user["first_name"]
+    assert response_body["weight"] == updated_record["weight"]
 
 
-  def test_delete(self, authenticated_client, user_factory):
-    user = user_factory.create()
-    user.save()
+  def test_delete(self, authenticated_client, health_metric_factory):
+    record = health_metric_factory.create()
+    record.save()
     
     response = authenticated_client.delete(
-      f"/api/users/{user.id}"
+      f"/api/health_metrics/{record.id}"
     )
     response_body = response.json()
 
